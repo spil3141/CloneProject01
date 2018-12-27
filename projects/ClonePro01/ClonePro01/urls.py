e991d3b82d613025de8e8b17d10dfc6259397d4a
spil3141 <johnj2015.jj22@gmail.com>
2018-12-27 21:07:58 +0900

    first commit

---
+++ projects/ClonePro01/ClonePro01/urls.py
@@ -0,0 +1,29 @@
+"""ClonePro01 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.conf.urls import url
+from django.urls import path,re_path,include
+# from Blog.views import Profile
+from django.contrib.auth import views
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    re_path(r"^",include("Blog.urls")),
+    url(r"accounts/login/$",views.login,name="Login_Page"),
+    url(r"accounts/logout/$",views.logout,name="Logout_Page",kwargs={"next_page":"/"}),
+
+    ]
